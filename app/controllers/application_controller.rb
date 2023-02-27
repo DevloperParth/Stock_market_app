3b9960eab21698342758d7c799951fa76ba8ac96
harshit sultaniya <harshit.sultaniya@preciousinfosystem.com>
2023-02-27 20:02:14 +0530

    User Profile Update Added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:fname, :lname,:contact_number, :email, :password, :password_configuration])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:fname, :lname,:contact_number, :email, :current_password, :password, :password_configuration])
   end
 end
