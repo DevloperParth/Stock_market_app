0cbf13c5e1ed200693446a7d9ea6a9a59177bfb3
harshit sultaniya <harshit.sultaniya@preciousinfosystem.com>
2023-02-28 14:03:25 +0530

    Dotenv Gem added

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,8 +74,8 @@ Rails.application.configure do
     address:              'smtp.gmail.com',
     port:                 587,
     domain:               'gmail.com',
-    user_name:            'harshit.sultaniya@preciousinfosystem.com',
-    password:             'qunnwpmuhczjopku',
+    user_name:            ENV['EMAIL'],
+    password:             ENV['PASSWORD'],
     authentication:       'plain',
     enable_starttls_auto: true  
  }
